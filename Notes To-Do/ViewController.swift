7afd6825aadad2d3a83ad2b24ed45f63f503de07
Clsz <muhammad.reynaldi001@binus.ac.id>
2019-06-30 16:38:38 +0700

    Initial Commit

---
+++ Notes To-Do/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes To-Do
+//
+//  Created by Muhammad Reynaldi on 30/06/19.
+//  Copyright © 2019 Cls. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
